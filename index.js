f8de32c0c51bb0e7acd8f0d2845473575914e823
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-17 00:14:31 +0100

    use morgan middleware for development

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const studentsRouter = require('./routes/students');
 const express = require('express');
+const morgan = require('morgan');
+const studentsRouter = require('./routes/students');
 const logger = require('./middlewares/logger');
 const autho = require('./middlewares/auth');
 const app = express();
@@ -8,7 +9,9 @@ const port = process.env.PORT || 3000 ;
 app.use(express.json());
 app.use(logger);
 app.use(autho);
-
+if(app.get('env') ==='development'){
+  app.use(morgan('tiny'))
+}
 /* routes */
 app.use('/api/students',studentsRouter);
 app.listen(port , ()=>{
