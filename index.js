562990a534937e64efc758a51242131072a0fb84
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-16 19:50:37 +0100

    change server port dynamically

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express()
+const port = process.env.PORT || 3000
+
+
 /* middleware */
+
  /* routes */
- app.get('/',(req,res)=>{
-     res.send('Hello word !')
+ app.get('/', (req, res) => {
+   res.send("Hello World")
  })
 
 
-
-
-
- app.listen(3000,()=>{
-   console.log('port Listening on localhost:3000');
+ app.listen(port,()=>{
+  console.log(`port Listening on localhost:${port}`);
  });
+
+
