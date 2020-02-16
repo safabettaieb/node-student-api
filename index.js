fc3e83fdcbf51686e1524fc294781a589ffbf80f
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-16 19:54:32 +0100

    get all students

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000
 
+/* Data local */
+students = [{
+    id: 1,
+    name: "name 1",
+    age: 25
+  },
+  {
+    id: 2,
+    name: "name 2",
+    age: 26
+  },
+  {
+    id: 3,
+    name: "name 3",
+    age: 27
+  }
+]
 
 /* middleware */
 
- /* routes */
- app.get('/', (req, res) => {
-   res.send("Hello World")
- })
+/* routes */
+app.get('/api/students', (req, res) => {
+  res.status(200)
+  res.json(students)
+});
 
 
- app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`port Listening on localhost:${port}`);
- });
-
-
+});
