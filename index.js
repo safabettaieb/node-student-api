54897397c0d52ab976f0debf3596f08604ee8e86
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-17 01:35:22 +0100

    use debug

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const config = require('config');
 const studentsRouter = require('./routes/students');
 const logger = require('./middlewares/logger');
 const autho = require('./middlewares/auth');
+const startUpDebugger = require('debug')('app:startup');
+const logDebugger = require('debug')('app:log');
 const app = express();
 const port = process.env.PORT || 3000 ;
+
+startUpDebugger('application started');
 /* middleware */
 app.use(express.json());
 app.use(logger);
 app.use(autho);
 if(app.get('env') ==='development'){
   app.use(morgan('tiny'))
+  logDebugger('morgan is enable');
 }
 console.log(`APP NAME: ${config.get('name')}`);
 /* routes */
